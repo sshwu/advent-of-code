e5045f6cd602de18f770d0dee77713df5543e402
Samuel Wu <wu.samuel.sh@gmail.com>
2020-12-11 08:24:23 -0800

    Day 8 Part 2

--- advent-of-code/HandheldHalting.py
+++ advent-of-code/HandheldHalting.py
@@ -1,17 +1,11 @@
 import re
 
-
-def runDayEight():
-    f = open("input8.txt","r")
-    txt = f.read()
-
-    instrRgx = "([a-z]{3}) ([+-][0-9]+)"
-    instrList = re.findall(instrRgx, txt)
+def execute(instrList, flipIdx):
     acc = 0
     visited = [False] * len(instrList)
     ptr = 0
-    halted = False
-    while not halted and not visited[ptr]:
+    valid = False
+    while not valid and not visited[ptr]:
         visited[ptr] = True
         instr = instrList[ptr][0]
         num = int(instrList[ptr][1])
@@ -19,9 +13,33 @@ def runDayEight():
             acc = acc + num
             ptr = ptr + 1
         elif instr == "jmp":
-            ptr = ptr + num
+            if ptr != flipIdx:
+                ptr = ptr + num
+            else:
+                ptr = ptr + 1
         elif instr == "nop":
-            ptr = ptr + 1
-        if ptr >= len(instrList):
-            halted = True
+            if ptr != flipIdx:
+                ptr = ptr + 1
+            else:
+                ptr = ptr + num
+        if ptr == len(instrList):
+            valid = True
+    return [valid, acc]
+
+def runDayEight():
+    f = open("input8.txt","r")
+    txt = f.read()
+
+    instrRgx = "([a-z]{3}) ([+-][0-9]+)"
+    instrList = re.findall(instrRgx, txt)
+    idx = 0
+    done = False
+    acc = -1
+    while not done and idx < len(instrList):
+        if instrList[idx][0] == "jmp" or instrList[idx][0] == "nop":
+            done, acc = execute(instrList, idx)
+            if not done:
+                idx = idx + 1
+        else:
+            idx = idx + 1
     print(acc)
